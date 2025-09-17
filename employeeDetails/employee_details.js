419b9082a8c506703f039fa3c3b4f6029e0f90b1
Daniel Barroso <barroso8292@gmail.com>
2025-09-17 14:44:05 -0400

    updated

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -1,7 +1,7 @@
 const employees = [
-      { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-      { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
-      { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
+      { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000, specialization: 'JavaScript' },
+      { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000, specialization: 'Python' },
+      { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000, specialization: 'Java' },
       //... More employee records can be added here
     ];
 
@@ -32,4 +32,10 @@ function findEmployeeById(employeeId) {
     else{
         document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this ID';
     }
+}
+
+function displayJSEmployees() {
+    const jsEmployees = employees.filter(employee => employee.specialization === 'JavaScript');
+    const jsEmployeesDisplay = jsEmployees.map((employee, index) => `<p>${employee.id}: ${employee.name} - ${employee.department} - ${employee.specialization}</p>`).join('');
+    document.getElementById('employeesDetails').innerHTML = jsEmployeesDisplay;
 }
